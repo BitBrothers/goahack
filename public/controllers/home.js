b97d42a2de34646212471d67b35dd925a53e386b
elroyalva <elroyalva197@hotmail.com>
2015-01-22 18:39:32 +0530

    padding fixes on members modal, create and register button fixed on landing page, other minor css changes

--- public/controllers/home.js
+++ public/controllers/home.js
@@ -1,5 +1,5 @@
 angular.module('GoaHack')
-  .controller('HomeCtrl', function($scope, $alert, $location, $http) {
+  .controller('HomeCtrl', function($scope, $alert, $location, $http, $rootScope) {
 
   $scope.register = function (){
     $location.path('/signup');
